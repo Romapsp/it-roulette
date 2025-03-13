c03f8aba1e29e66ab8939a165965b5bddfa54fc2
noobprofesor <noobprofesor@gmail.com>
2025-03-13 09:11:06 +0300

    HTML for prizes list changed

--- src/app/components/prizes-flex/prizes-flex.component.ts
+++ src/app/components/prizes-flex/prizes-flex.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { prize } from '../../interfaces/prize.interface';
 import { PrizeComponent } from "../prize/prize.component";
 import { CommonModule } from '@angular/common';
+import { servise } from '../../interfaces/srvise.interface';
 
 @Component({
   selector: 'app-prizes-flex',
@@ -11,5 +12,13 @@ import { CommonModule } from '@angular/common';
 })
 export class PrizesFlexComponent {
   @Input() prizes!: prize[];
-  @Input() flexDirection?: string = 'horizontal';  
+  @Input() flexDirection?: string = 'horizontal';
+  serviseArr: servise[] = [];
+
+  ngOnChanges() {
+    for (let i = 0; i < Math.ceil(30 / this.prizes.length); i++) {
+      this.serviseArr.push({ id: i });
+    }
+  }
+
 }
